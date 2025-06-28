0a5481b0a9cf565606579ed2df7bb53eaa5dfc4d
thetrickuser <coolady1807@outlook.com>
2025-06-28 13:30:15 +0000

    responsive design changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,12 @@ import {Briefcase, MessageSquare, Rss, Settings, User} from "lucide-react";
 type NavbarProps = {
     onClick: (sectionId: React.SetStateAction<string>) => void;
     activeSection: string;
+    style: string;
 }
 
-export const Navbar = ({ onClick, activeSection }: NavbarProps ) => {
-    return <nav className="hidden md:block absolute top-6 right-6">
-        <ul className="flex bg-gray-200 dark:bg-gray-700 p-2 rounded-full shadow-inner">
+export const Navbar = ({ onClick, activeSection, style }: NavbarProps ) => {
+    return <nav className={`${style}`}>
+        <ul className="flex flex-wrap bg-gray-200 dark:bg-gray-700 p-2 shadow-inner justify-center xl:rounded-bl-2xl xl:rounded-tr-2xl rounded-t-2xl xl:rounded-none">
             <NavigationItem onClick={onClick} activeSection={activeSection} title={"About"}
                             section={"about"} icon={<User size={16} className="mr-1"/>}/>
             <NavigationItem onClick={onClick} activeSection={activeSection} title={"Resume"}
